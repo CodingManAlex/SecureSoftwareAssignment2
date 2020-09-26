65cbf4d1403005de6b99157b81923c9b4597d12c
Alex Watson <alexander.watson1@me.com>
2020-09-26 18:21:40 -0400

    Initial Commit

---
+++ SSD-Lab1/SSD-Lab1/Models/ApplicationUser.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SSD_Lab1.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required, Display(Name ="First Name")]
+        public string FirstName { get; set; }
+        [Required, Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        //[Required]
+        //public string Email { get; set; }
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+        [Display(Name = "Date of Birth")]
+        public DateTime DateOfBirth { get; set; }
+    }
+}
